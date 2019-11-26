3890452ecf0e918b96c9855f6c0981d2fbb11c35
Hugo Bove <hugo.bove@neotechnology.com>
2019-11-26 13:41:54 +0100

    lint

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,7 @@
-import {app, BrowserWindow} from "electron";
-import * as path from "path";
+import {app, BrowserWindow} from 'electron';
+import * as path from 'path';
 
-import {
-    addViewtron
-} from 'viewtron/dist/ipc-main';
+import {addViewtron} from 'viewtron/dist/ipc-main';
 
 let mainWindow: Electron.BrowserWindow;
 
@@ -12,19 +10,19 @@ function createWindow() {
     mainWindow = new BrowserWindow({
         height: 600,
         webPreferences: {
-            preload: path.join(__dirname, "preload.js"),
+            preload: path.join(__dirname, 'preload.js'),
         },
         width: 800,
     });
 
     // and load the index.html of the app.
-    mainWindow.loadFile(path.join(__dirname, "../views/index.html"));
+    mainWindow.loadFile(path.join(__dirname, '../views/index.html'));
 
     // Open the DevTools.
     // mainWindow.webContents.openDevTools({mode: 'undocked'});
 
     // Emitted when the window is closed.
-    mainWindow.on("closed", () => {
+    mainWindow.on('closed', () => {
         // Dereference the window object, usually you would store windows
         // in an array if your app supports multi windows, this is the time
         // when you should delete the corresponding element.
@@ -38,24 +36,24 @@ function createWindow() {
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on("ready", createWindow);
+app.on('ready', createWindow);
 
 // Quit when all windows are closed.
-app.on("window-all-closed", () => {
+app.on('window-all-closed', () => {
     // On OS X it is common for applications and their menu bar
     // to stay active until the user quits explicitly with Cmd + Q
-    if (process.platform !== "darwin") {
+    if (process.platform !== 'darwin') {
         app.quit();
     }
 });
 
-app.on("activate", () => {
-    // On OS X it"s common to re-create a window in the app when the
+app.on('activate', () => {
+    // On OS X it's common to re-create a window in the app when the
     // dock icon is clicked and there are no other windows open.
     if (mainWindow === null) {
         createWindow();
     }
 });
 
-// In this file you can include the rest of your app"s specific main process
+// In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
--- src/preload.ts
+++ src/preload.ts
@@ -1,5 +1,5 @@
-import {ResizeSensor} from "css-element-queries";
-import {ViewtronUpdateData} from "viewtron";
+import {ResizeSensor} from 'css-element-queries';
+import {ViewtronUpdateData} from 'viewtron';
 import {
     addColumnHandler,
     addRowHandler,
@@ -22,11 +22,11 @@ import {
     viewtronResizeHandler,
     viewtronUpdateHandler,
     viewVisibilityHandler,
-} from "viewtron/dist/ipc-renderer";
+} from 'viewtron/dist/ipc-renderer';
 
 // All of the Node.js APIs are available in the preload process.
 // It has the same sandbox as a Chrome extension.
-window.addEventListener("DOMContentLoaded", () => {
+window.addEventListener('DOMContentLoaded', () => {
     const replaceText = (selector: string, text: string) => {
         const element = document.getElementById(selector);
         if (element) {
@@ -34,11 +34,11 @@ window.addEventListener("DOMContentLoaded", () => {
         }
     };
 
-    for (const type of ["chrome", "node", "electron"]) {
+    for (const type of ['chrome', 'node', 'electron']) {
         replaceText(`${type}-version`, (process.versions as any)[type]);
     }
 
-    const appArea = document.getElementById("app-area");
+    const appArea = document.getElementById('app-area');
 
     requestAnimationFrame(() => {
         // @ts-ignore
@@ -54,37 +54,37 @@ window.addEventListener("DOMContentLoaded", () => {
         // @ts-ignore
         window.currentViews = views;
 
-        document.getElementById("sidebar-list").innerHTML = `
+        document.getElementById('sidebar-list').innerHTML = `
             ${rows.map(({id}) => {
             const rowColumns = columns.filter(({rowId}) => rowId === id);
 
             return `
                     <li>
-                        Row (<span class="value">${id}</span>) <button data-row-id="${id}">-</button>
+                        Row (<span class='value'>${id}</span>) <button data-row-id='${id}'>-</button>
                         <ul>
                             ${rowColumns.map((column) => {
                 const colViews = views.filter((view) => view.columnId === column.id);
 
                 return `
                                     <li>
-                                        Column (<span class="value">${column.id}</span>) <button data-column-id="${column.id}">-</button>
+                                        Column (<span class='value'>${column.id}</span>) <button data-column-id='${column.id}'>-</button>
                                         <ul>
                                             ${colViews.map(({id: viewId, url}) => `
-                                                <li><span class="value">${url}</span> (<span class="value">${viewId}</span>) <button data-view-id="${viewId}">-</button></li>
-                                            `).join("")}
+                                                <li><span class='value'>${url}</span> (<span class='value'>${viewId}</span>) <button data-view-id='${viewId}'>-</button></li>
+                                            `).join('')}
                                         </ul>
                                     </li>
                                 `;
-            }).join("")}
+            }).join('')}
                         </ul>
                     </li>
                 `;
-        }).join("")}
+        }).join('')}
         `;
     });
 
-    document.getElementById("sidebar-list").addEventListener("click", (event: any) => {
-        if (event.target.nodeName !== "BUTTON") {
+    document.getElementById('sidebar-list').addEventListener('click', (event: any) => {
+        if (event.target.nodeName !== 'BUTTON') {
             return;
         }
 
@@ -105,179 +105,179 @@ window.addEventListener("DOMContentLoaded", () => {
         }
     });
 
-    document.getElementById("add-row").addEventListener("click", () => {
+    document.getElementById('add-row').addEventListener('click', () => {
         addRowHandler({});
     });
 
-    document.getElementById("add-column-form").addEventListener("submit", (event: any) => {
+    document.getElementById('add-column-form').addEventListener('submit', (event: any) => {
         event.preventDefault();
 
         const data = new FormData(event.target);
-        const rowId = String(data.get("rowId"));
+        const rowId = String(data.get('rowId'));
 
         addColumnHandler({rowId});
 
         // @ts-ignore
-        document.getElementById("add-column-form-row-id-input").value = "";
+        document.getElementById('add-column-form-row-id-input').value = '';
     }, false);
 
-    document.getElementById("add-view-form").addEventListener("submit", (event: any) => {
+    document.getElementById('add-view-form').addEventListener('submit', (event: any) => {
         event.preventDefault();
 
         const data = new FormData(event.target);
-        const url = data.get("url") as string;
-        const columnId = String(data.get("columnId"));
+        const url = data.get('url') as string;
+        const columnId = String(data.get('columnId'));
 
         addViewHandler({url, columnId});
 
         // @ts-ignore
-        document.getElementById("add-view-form-url-input").value = "";
+        document.getElementById('add-view-form-url-input').value = '';
         // @ts-ignore
-        document.getElementById("add-view-form-column-id-input").value = "";
+        document.getElementById('add-view-form-column-id-input').value = '';
     }, false);
 
-    document.getElementById("row-height-form").addEventListener("submit", (event: any) => {
+    document.getElementById('row-height-form').addEventListener('submit', (event: any) => {
         event.preventDefault();
 
         const data = new FormData(event.target);
-        const rowId = data.get("rowId") as string;
-        const height = Number(data.get("height"));
+        const rowId = data.get('rowId') as string;
+        const height = Number(data.get('height'));
 
         rowResizeHandler({rowId, height});
 
         // @ts-ignore
-        document.getElementById("row-height-form-row-id-input").value = "";
+        document.getElementById('row-height-form-row-id-input').value = '';
         // @ts-ignore
-        document.getElementById("row-height-form-row-height-input").value = "";
+        document.getElementById('row-height-form-row-height-input').value = '';
     }, false);
 
-    document.getElementById("column-width-form").addEventListener("submit", (event: any) => {
+    document.getElementById('column-width-form').addEventListener('submit', (event: any) => {
         event.preventDefault();
 
         const data = new FormData(event.target);
-        const columnId = data.get("columnId") as string;
-        const width = Number(data.get("width"));
+        const columnId = data.get('columnId') as string;
+        const width = Number(data.get('width'));
 
         columnResizeHandler({columnId, width});
 
         // @ts-ignore
-        document.getElementById("column-width-form-column-id-input").value = "";
+        document.getElementById('column-width-form-column-id-input').value = '';
         // @ts-ignore
-        document.getElementById("column-width-form-column-width-input").value = "";
+        document.getElementById('column-width-form-column-width-input').value = '';
     }, false);
 
-    document.getElementById("view-height-form").addEventListener("submit", (event: any) => {
+    document.getElementById('view-height-form').addEventListener('submit', (event: any) => {
         event.preventDefault();
 
         const data = new FormData(event.target);
-        const viewId = data.get("viewId") as string;
-        const height = Number(data.get("height"));
+        const viewId = data.get('viewId') as string;
+        const height = Number(data.get('height'));
 
         viewResizeHandler({viewId, height});
 
         // @ts-ignore
-        document.getElementById("view-height-form-view-id-input").value = "";
+        document.getElementById('view-height-form-view-id-input').value = '';
         // @ts-ignore
-        document.getElementById("view-height-form-view-height-input").value = "";
+        document.getElementById('view-height-form-view-height-input').value = '';
     }, false);
 
     // @ts-ignore
     document.querySelector('#row-visibility-form .default').checked = 'checked';
-    document.getElementById("row-visibility-form").addEventListener("submit", (event: any) => {
+    document.getElementById('row-visibility-form').addEventListener('submit', (event: any) => {
         event.preventDefault();
 
         const data = new FormData(event.target);
-        const rowId = data.get("rowId") as string;
-        const visible = data.get("visibility") === 'visible';
+        const rowId = data.get('rowId') as string;
+        const visible = data.get('visibility') === 'visible';
 
         rowVisibilityHandler({rowId, visible});
 
         // @ts-ignore
-        document.getElementById("row-visibility-form-row-id-input").value = "";
+        document.getElementById('row-visibility-form-row-id-input').value = '';
         // @ts-ignore
         document.querySelector('#row-visibility-form .default').checked = 'checked';
     }, false);
 
     // @ts-ignore
     document.querySelector('#column-visibility-form .default').checked = 'checked';
-    document.getElementById("column-visibility-form").addEventListener("submit", (event: any) => {
+    document.getElementById('column-visibility-form').addEventListener('submit', (event: any) => {
         event.preventDefault();
 
         const data = new FormData(event.target);
-        const columnId = data.get("columnId") as string;
-        const visible = data.get("visibility") === 'visible';
+        const columnId = data.get('columnId') as string;
+        const visible = data.get('visibility') === 'visible';
 
         columnVisibilityHandler({columnId, visible});
 
         // @ts-ignore
-        document.getElementById("column-visibility-form-column-id-input").value = "";
+        document.getElementById('column-visibility-form-column-id-input').value = '';
         // @ts-ignore
         document.querySelector('#column-visibility-form .default').checked = 'checked';
     }, false);
 
     // @ts-ignore
     document.querySelector('#view-visibility-form .default').checked = 'checked';
-    document.getElementById("view-visibility-form").addEventListener("submit", (event: any) => {
+    document.getElementById('view-visibility-form').addEventListener('submit', (event: any) => {
         event.preventDefault();
 
         const data = new FormData(event.target);
-        const viewId = data.get("viewId") as string;
-        const visible = data.get("visibility") === 'visible';
+        const viewId = data.get('viewId') as string;
+        const visible = data.get('visibility') === 'visible';
 
         viewVisibilityHandler({viewId, visible});
 
         // @ts-ignore
-        document.getElementById("view-visibility-form-view-id-input").value = "";
+        document.getElementById('view-visibility-form-view-id-input').value = '';
         // @ts-ignore
         document.querySelector('#view-visibility-form .default').checked = 'checked';
     }, false);
 
-    document.getElementById("row-move-form").addEventListener("submit", (event: any) => {
+    document.getElementById('row-move-form').addEventListener('submit', (event: any) => {
         event.preventDefault();
 
         const data = new FormData(event.target);
-        const rowId = data.get("rowId") as string;
-        const newIndex = Number(data.get("newIndex"));
+        const rowId = data.get('rowId') as string;
+        const newIndex = Number(data.get('newIndex'));
 
         reorderRowHandler({rowId, newIndex});
 
         // @ts-ignore
-        document.getElementById("row-move-form-row-id-input").value = "";
+        document.getElementById('row-move-form-row-id-input').value = '';
         // @ts-ignore
-        document.getElementById("row-move-form-row-new-index-input").value = "";
+        document.getElementById('row-move-form-row-new-index-input').value = '';
     }, false);
 
-    document.getElementById("column-move-form").addEventListener("submit", (event: any) => {
+    document.getElementById('column-move-form').addEventListener('submit', (event: any) => {
         event.preventDefault();
 
         const data = new FormData(event.target);
-        const columnId = data.get("columnId") as string;
-        const newIndex = Number(data.get("newIndex"));
+        const columnId = data.get('columnId') as string;
+        const newIndex = Number(data.get('newIndex'));
 
         reorderColumnHandler({columnId, newIndex});
 
         // @ts-ignore
-        document.getElementById("column-move-form-column-id-input").value = "";
+        document.getElementById('column-move-form-column-id-input').value = '';
         // @ts-ignore
-        document.getElementById("column-move-form-column-new-index-input").value = "";
+        document.getElementById('column-move-form-column-new-index-input').value = '';
     }, false);
 
-    document.getElementById("view-move-form").addEventListener("submit", (event: any) => {
+    document.getElementById('view-move-form').addEventListener('submit', (event: any) => {
         event.preventDefault();
 
         const data = new FormData(event.target);
-        const viewId = data.get("viewId") as string;
-        const newIndex = Number(data.get("newIndex"));
+        const viewId = data.get('viewId') as string;
+        const newIndex = Number(data.get('newIndex'));
 
         reorderViewHandler({viewId, newIndex});
 
         // @ts-ignore
-        document.getElementById("view-move-form-view-id-input").value = "";
+        document.getElementById('view-move-form-view-id-input').value = '';
         // @ts-ignore
-        document.getElementById("view-move-form-view-new-index-input").value = "";
+        document.getElementById('view-move-form-view-new-index-input').value = '';
     }, false);
 
-    document.getElementById("reset-views").addEventListener("click", () => {
+    document.getElementById('reset-views').addEventListener('click', () => {
         rowResetHandler({});
         columnResetHandler({});
         viewResetHandler({});
